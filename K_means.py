a3fcc2004c7b797a709ddbfc81d75a28f5f79da3
GonzaNunezCano <gonzalo.nunez.cano@gmail.com>
2019-04-26 18:30:08 -0300

    k means ready

--- K_means.py
+++ K_means.py
@@ -11,20 +11,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import random
+import sys
 
-def init_random_centroids():
+def init_random_centroids(n_clusters):
+  random_centroids = {}
+  for i in range(0,n_clusters):
+    centroid = []
+    for j in range(0,26):
+      centroid.append(random.randint(1,5))
+    random_centroids[i] = centroid
+  return random_centroids
 
-def init_heuristic_centroids():
 
-def add_to_nearest_centroid(centroids, dataset):
-  for row in dataset.iterrows():
+#def init_heuristic_centroids(n_clusters):
 
+def init_empty_clusters(n_clusters):
+  clusters = {}
+  for i in range(0, n_clusters):
+    clusters[i] = []
+
+  return clusters
+
+def add_to_nearest_cluster(centroids, dataset):
+  clusters = init_empty_clusters(len(centroids.keys()))
+  print(clusters)
+  dataset_list = dataset.apply(lambda x: x.tolist(), axis = 1)
+  for i in range(0, len(dataset_list)):
+    v = np.array(dataset_list[i])
+    distances = []
+    for centroid in centroids:
+      u = np.array(centroid)
+      dist = np.linalg.norm(v-u)
+      distances.append(dist)
+    cluster = distances.index(min(distances))
+    print(cluster)
+    print(type(cluster))
+    tmp = clusters[cluster].append(dataset_list[i])
+    clusters[cluster] = tmp
+
+  return clusters
+
+def recalculate_centroids(clusters, n_clusters):
+  new_centroids = {}
+  for i in range(0, n_clusters):
+    cluster_df = pd.DataFrame(clusters[i])
+    mean = cluster_df.mean()
+    new_centroids[i] = list(map(lambda x: x, mean))
+
+  return new_centroids
 
 def k_means(n_clusters, init_centroids):
   dataset = pd.read_csv('data.csv')
   dataset = dataset.iloc[:, range(2,28)]
 
   if init_centroids == 'random':
-    init_random_centroids()
+    centroids = init_random_centroids(n_clusters)
   else:
-    init_heuristic_centroids()
+    centroids = init_heuristic_centroids(n_clusters)
+
+  while True:
+    clusters = add_to_nearest_cluster(centroids, dataset)
+    new_centroids = recalculate_centroids(clusters, n_clusters)
+    if centroids == new_centroids:
+      break
+    else:
+      centroids = new_centroids.copy()
+
+  return clusters
+
+if __name__ == "__main__":
+  n_clusters = int(sys.argv[1])
+  init_centroids = sys.argv[2]
+
+  clusters = k_means(n_clusters, init_centroids)
+  print(clusters)
