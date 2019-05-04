d72311946fc14a3714d605e752f1dbc24b20d72a
joaguirrem <joaquin.aguirre@fing.edu.uy>
2019-05-04 20:34:23 -0300

    Fix PCA

--- a/PCA_new.py
+++ a/PCA_new.py
@@ -4,16 +4,14 @@ import sys
 from matplotlib import pyplot as plt
 
 
-import pdb
-
-
-def get_plot(index):
+def get_plot_values(value):
     switcher = {
+        # Frente Amplio
         1: ["s", "blue"],
         2: ["s", "blue"],
         3: ["s", "blue"],
         4: ["s", "blue"],
-
+        # Partido Nacional
         5: ["^", "lightblue"],
         6: ["^", "lightblue"],
         7: ["^", "lightblue"],
@@ -21,33 +19,33 @@ def get_plot(index):
         9: ["^", "lightblue"],
         10: ["^", "lightblue"],
         11: ["^", "lightblue"],
-
+        # Partido Colorado
         12: ["o", "red"],
         13: ["o", "red"],
         14: ["o", "red"],
         15: ["o", "red"],
         16: ["o", "red"],
         17: ["o", "red"],
-
+        # La Alternativa
         18: ["d", "purple"],
-
+        # Unidad Popular
         19: ["+", "black"],
-
+        # Partido de la Gente
         20: ["*", "green"],
-
+        # PERI
         21: ["x", "orange"],
-
+        # Partido de los Trabajadores
         22: ["D", "yellow"],
-
+        # Partido Digital
         23: ["X", "lightgreen"],
-
+        # Partido Verde
         24: ["1", "brown"],
-
+        # Partido de Todos
         25: ["P", "grey"],
     }
     len_switcher = len(switcher) + 1
-    shape = switcher.get(index % len_switcher, ["-", "black"])[0]
-    color = switcher.get(index % len_switcher, ["-", "black"])[1]
+    shape = switcher.get(value % len_switcher, ["-", "black"])[0]
+    color = switcher.get(value % len_switcher, ["-", "black"])[1]
     return shape, color
 
 
@@ -82,18 +80,16 @@ if __name__ == "__main__":
 
     transformed = np.dot(datamatrix.T, matrix_w).transpose()
 
-    pdb.set_trace()
-
-    counter = 1
+    value = 1
     prev_index = 0
     index = 0
     for candidate in candidates:
         index += candidate
-        shape, color = get_plot(counter)
+        shape, color = get_plot_values(value)
         plt.plot(transformed[0, prev_index:index], transformed[1, prev_index:index],
                  shape, markersize=7, color=color, alpha=0.5, label=candidate)
         prev_index = index
-        counter += 1
+        value += 1
 
     plt.xlabel("x_values")
     plt.ylabel("y_values")
